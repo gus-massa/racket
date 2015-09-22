65861a091a73ec3ee99885267b881d8e8a87fc5e
Gustavo Massaccesi <gustavo@oma.org.ar>
2015-09-21 23:48:34 -0300

    member2x

--- racket/collects/racket/private/member2.rkt
+++ racket/collects/racket/private/member2.rkt
@@ -2,7 +2,7 @@
   (#%require "cond.rkt" "qq-and-or.rkt"
              "performance-hint.rkt"
              (for-syntax '#%kernel "qq-and-or.rkt"))
-  (#%provide memq2 memv2 member2)
+  (#%provide memq2 memq2x memv2 member2 member2x)
 
   ;; helper for memq/v/ber error cases
   (define-values (bad-list)
@@ -29,18 +29,34 @@
                             [else (loop (cdr ls))])))])
           ,id))))
 
-  ;; Create the `member` function that takes an extra argument
-  ;; Uses `mk` to construct the body
-  (define-syntaxes (mk-member)
+  (define-syntaxes (mk3)
     (λ (stx)
       (define-values (forms) (syntax-e stx))
-      (define-values (id) (syntax-e (cadr forms)))
+      (define-values (id)
+        (values (syntax-e (cadr forms))))
       (datum->syntax
        stx
-       `(let* ([default (mk member equal?)]
-               [,id (case-lambda
-                      ([v orig-l] (default v orig-l))
-                      ([v orig-l eq?]
+       `(let-values ([(,id)
+                      (lambda (v orig-l eq?)
+                        (let loop ([ls orig-l])
+                          (cond
+                            [(null? ls) #f]
+                            [(not (pair? ls))
+                             (bad-list ',id orig-l)]
+                            [(,eq? v (car ls)) ls]
+                            [else (loop (cdr ls))])))])
+          ,id))))
+
+  (define-values (memq2x) (mk memq eq?))
+  (begin-encourage-inline
+    (define-values (memq2) (mk memq eq?))
+    (define-values (memv2) (mk memq eqv?))
+    ;; Note that this uses `mk-member`
+    (define-values (member2) (mk member equal?))
+    (define-values (member/default) (mk member equal?))
+    (define-values (member/any) (mk3 member))
+    (define-values (member/test)
+                    (lambda (v orig-l eq?) 
                        (if (and (procedure? eq?)
                                 (procedure-arity-includes? eq? 2))
                            (void)
@@ -48,12 +64,8 @@
                             'member
                             "(procedure-arity-includes/c 2)"
                             eq?))
-                       ((mk member eq?) v orig-l)))])
-          ,id))))
-
-  (begin-encourage-inline
-    (define-values (memq2) (mk memq eq?))
-    (define-values (memv2) (mk memq eqv?))
-    ;; Note that this uses `mk-member`
-    #;(define-values (member2) (mk-member member))
-    (define-values (member2) (mk member equal?)))) 
+                       (member/any v orig-l eq?)))  
+    (define-values (member2x)
+                    (case-lambda
+                      ([v orig-l] (member/default v orig-l))
+                      ([v orig-l eq?] (member/test v orig-l eq?))))))
